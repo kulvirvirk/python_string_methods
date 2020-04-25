afadd026e825d3b77aa4ef21ea94d265065ca923
kulvirvirk <kvirkdev@gmail.com>
2020-04-25 06:39:53 +0000

    added some lorem ipsum text

---
+++ main.py
@@ -0,0 +1,6 @@
+
+#
+# upper() - Call this method to return an upper case version of any string.
+# lower() - Call this method to return a lower case version of any string. 
+
+someString = "Lorem ipsum dolor sit amet, an postulant conclusionemque eum. Ex deleniti pertinax est, mei probo euismod intellegebat at. Fierent indoctum vel cu. Qui graeci maiorum nominavi ei, ex nonumy ponderum duo. Atqui quidam mea te. Ius accusamus cotidieque at, et duo aliquip dolores, munere iudicabit expetendis eu his. Ignota delicatissimi pri ne, magna harum te est, cu eum laudem legendos."
