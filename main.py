7b08480f70e7225a4b7e106e663a1d5b1f66dce4
kulvirvirk <kvirkdev@gmail.com>
2020-04-26 05:48:54 +0000

    added a comparison example

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-
-#
 # 1. upper() - Call this method to return an upper case version of any string.
 # 2. lower() - Call this method to return a lower case version of any string. 
+# 3. provide comparision example using a .lower() method
 
-someString = "Lorem ipsum dolor sit amet, an postulant conclusionemque eum. Ex deleniti pertinax est, mei probo euismod intellegebat at. Fierent indoctum vel cu. Qui graeci maiorum nominavi ei, ex nonumy ponderum duo. Atqui quidam mea te. Ius accusamus cotidieque at, et duo aliquip dolores, munere iudicabit expetendis eu his. Ignota delicatissimi pri ne, magna harum te est, cu eum laudem legendos."
+some_string = "Lorem ipsum dolor sit amet, an postulant conclusionemque eum. Ex deleniti pertinax est, mei probo euismod intellegebat at. Fierent indoctum vel cu. Qui graeci maiorum nominavi ei, ex nonumy ponderum duo. Atqui quidam mea te. Ius accusamus cotidieque at, et duo aliquip dolores, munere iudicabit expetendis eu his. Ignota delicatissimi pri ne, magna harum te est, cu eum laudem legendos."
 
 # 1. upper() - Call this method to return an upper case version of any string.
-print(someString.upper())
+print(some_string.upper())
 print('===============================')
 
 # note: the original string is still the same, 
-print(someString)
+print(some_string)
 print('===============================')
 
 # 2. lower() - Call this method to return a lower case version of any string. 
-print(someString.lower())
+print(some_string.lower())
 print('===============================')
+
+# 3. provide comparision example using a .lower() method
+'''user can enter any value(uppercase or lowercase) in form of input, logic should be able to compare it'''
+
+user_input = input("Enter 'Yes' or 'No' to continue:")
+print(user_input)
+print(user_input.lower())
