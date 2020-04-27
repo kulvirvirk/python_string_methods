4c53bd4ea23dfe1b29326eb12a8a7364b4cd13c5
kulvirvirk <kvirkdev@gmail.com>
2020-04-27 03:15:30 +0000

    added a example of islower() method

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # 1. upper() - Call this method to return an upper case version of any string.
 # 2. lower() - Call this method to return a lower case version of any string. 
 # 3. provide comparision example using a .lower() method
+# 4. islower() - call this method to check if string is lowercase or not
+# 4. isupper() - call this method to check if string is uppercase or not
 
 some_string = "Lorem ipsum dolor sit amet, an postulant conclusionemque eum. Ex deleniti pertinax est, mei probo euismod intellegebat at. Fierent indoctum vel cu. Qui graeci maiorum nominavi ei, ex nonumy ponderum duo. Atqui quidam mea te. Ius accusamus cotidieque at, et duo aliquip dolores, munere iudicabit expetendis eu his. Ignota delicatissimi pri ne, magna harum te est, cu eum laudem legendos."
 
@@ -23,3 +25,9 @@ user_input = input('Play again(Yes/No): ')
 while user_input.lower() == 'yes':
   user_input = input('Play again(Yes/No): ')
 print('Ok, bye!')
+print('===============================')
+
+# 4. islower() - call this method to check if string is lowercase or not
+print (some_string.islower())
+some_string = some_string.lower()
+print (some_string.islower())
