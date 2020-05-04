30db6d26ede765aea4c8e43919616bb409f2ca02
kulvirvirk <kvirkdev@gmail.com>
2020-05-04 06:12:12 +0000

    added an example of count() method

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-# 1. upper() - Call this method to return an upper case version of any string.
-# 2. lower() - Call this method to return a lower case version of any string. 
-# 3. provide comparision example using a .lower() method
-# 4. islower() - call this method to check if string is lowercase or not
-# 5. isupper() - call this method to check if string is uppercase or not
+# 1. count()  - call t his method to count specific char in string
+# 2. upper() - Call this method to return an upper case version of any string.
+# 3. lower() - Call this method to return a lower case version of any string. 
+# 4. provide comparision example using a .lower() method
+# 5. islower() - call this method to check if string is lowercase or not
+# 6. isupper() - call this method to check if string is uppercase or not
+
 
 some_string = "Lorem ipsum dolor sit amet, an postulant conclusionemque eum. Ex deleniti pertinax est, mei probo euismod intellegebat at. Fierent indoctum vel cu. Qui graeci maiorum nominavi ei, ex nonumy ponderum duo. Atqui quidam mea te. Ius accusamus cotidieque at, et duo aliquip dolores, munere iudicabit expetendis eu his. Ignota delicatissimi pri ne, magna harum te est, cu eum laudem legendos."
 
-# 1. upper() - Call this method to return an upper case version of any string.
+# 1. count()  - call t his method to count specific char in string
+print('there are ' + str(some_string.count('a')) + '\'a\'found in given text')
+print('===============================')
+
+# 2. upper() - Call this method to return an upper case version of any string.
 print(some_string.upper())
 print('===============================')
 
@@ -14,11 +20,11 @@ print('===============================')
 print(some_string)
 print('===============================')
 
-# 2. lower() - Call this method to return a lower case version of any string. 
+# 3. lower() - Call this method to return a lower case version of any string. 
 print(some_string.lower())
 print('===============================')
 
-# 3. provide comparison example using a .lower() method
+# 4. provide comparison example using a .lower() method
 '''user can enter any value(uppercase or lowercase) in form of input, logic should be able to compare it'''
 
 user_input = input('Play again(Yes/No): ')
@@ -27,7 +33,7 @@ while user_input.lower() == 'yes':
 print('Ok, bye!')
 print('===============================')
 
-# 4. islower() - call this method to check if string is lowercase or not
+# 5. islower() - call this method to check if string is lowercase or not
 print (some_string.islower())
  #now change the string to lowercase using the lower() method
 some_string = some_string.lower()
@@ -35,10 +41,13 @@ some_string = some_string.lower()
 print (some_string.islower())
 print('===============================')
 
-# 5. isupper() - call this method to check if string is uppercase or not
+# 6. isupper() - call this method to check if string is uppercase or not
 print (some_string.isupper())
  #now change the string to uppercase using the upper() method
 some_string = some_string.upper()
  #check again
 print (some_string.isupper())
 print('===============================')
+
+
+
